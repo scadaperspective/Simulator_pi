2d7909dbf611e4ad658757306d0bd9a331c0f324
Rick Gleason <fcgleason@tgp-architects.com>
2019-06-21 18:35:11 -0400

    remove build files

--- build/CMakeFiles/create-pkg.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/create-pkg"
-  "Simulator-Plugin.pkg"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/create-pkg.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
